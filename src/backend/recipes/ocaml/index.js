2b58105ed0e1822aa13c4a4a09d2a140e048cd4e
Peter Squicciarini <stripedpajamas273@gmail.com>
2018-10-06 22:40:26 -0400

    Add OCaml support

---
+++ src/backend/recipes/ocaml/index.js
@@ -0,0 +1,35 @@
+const { defaultInitNotebook } = require('../defaultInitNotebook');
+const stdExec = require('../../stdexec');
+const stdExecDocker = require('../../stdexecdocker');
+
+const recipe = ({
+    key: 'ocaml',
+    name: 'OCaml',
+    language: 'OCaml',
+    mainfile: ['index.ml', 'main.ml'],
+    cmmode: 'ocaml',
+    dir: __dirname,
+    exec: ({ notebook, docker, writeStdOut, writeStdErr, writeInfo, env }) => {
+
+        if (docker) {
+            return stdExecDocker({
+                image: 'ocaml/opam:alpine',
+                cmd: ['sh', '-c', "ocamlc -o /tmp/code.out /code/" + notebook.mainfilename + " && /tmp/code.out"],
+                cwd: '/code',
+                mounts: [
+                    { from: notebook.absdir, to: '/code', mode: 'rw' },
+                ],
+                env,
+            }, writeStdOut, writeStdErr, writeInfo);
+        } else {
+            return stdExec({
+                cmd: ['sh', '-c', "ocamlc -o /tmp/code.out " + notebook.mainfilename + " && /tmp/code.out"],
+                cwd: notebook.absdir,
+                env,
+            }, writeStdOut, writeStdErr, writeInfo);
+        }
+    },
+    init: async ({ name, notebookspath }) => await defaultInitNotebook(recipe, notebookspath, name),
+});
+
+module.exports = recipe;
