1ceb1afed6e769067ab00063408b3619065b3ad7
Jerome Schneider <jerome.schneider@algolia.com>
2020-02-25 23:14:20 +0100

    Fixing relative path handling; related to #80

--- main.go
+++ main.go
@@ -3,10 +3,12 @@ package main
 import (
 	"fmt"
 	"os"
+	"path/filepath"
 
 	"github.com/alecthomas/kingpin"
 	"github.com/markbates/pkger"
 	"github.com/netgusto/nodebook/src/core"
+	"github.com/pkg/errors"
 )
 
 var _ = pkger.Include("/dist/frontend/")
@@ -25,31 +27,38 @@ func main() {
 	cliCmdDocker := cliCmd.Flag("docker", "Use docker").Bool()
 
 	args := os.Args[1:]
+	if len(args) == 0 || (args[0] != webCmd.FullCommand() && args[0] != cliCmd.FullCommand()) {
+		args = append([]string{"web"}, args...)
+	}
 	selected, err := kingpin.CommandLine.Parse(args)
 	if err != nil {
-		if len(args) == 0 {
-			fmt.Println(err)
-			os.Exit(1)
-		}
-
-		if args[0] != webCmd.FullCommand() && args[0] != cliCmd.FullCommand() {
-			args = append([]string{"web"}, args...)
-		} else {
-			fmt.Println(err)
-			os.Exit(1)
-		}
-
-		selected, err = kingpin.CommandLine.Parse(args)
-		if err != nil {
-			fmt.Println(err)
-			os.Exit(1)
-		}
+		fmt.Println(err)
+		os.Exit(1)
 	}
 
 	switch selected {
 	case webCmd.FullCommand():
-		core.WebRun(*webCmdPath, *webCmdDocker, *webCmdBindAddress, *webCmdPort)
+		absPath, err := absolutizePath(*webCmdPath)
+		if err != nil {
+			fmt.Println(err)
+			os.Exit(1)
+		}
+		core.WebRun(absPath, *webCmdDocker, *webCmdBindAddress, *webCmdPort)
 	case cliCmd.FullCommand():
-		core.CliRun(*cliCmdPath, *cliCmdDocker)
+		absPath, err := absolutizePath(*cliCmdPath)
+		if err != nil {
+			fmt.Println(err)
+			os.Exit(1)
+		}
+		core.CliRun(absPath, *cliCmdDocker)
 	}
+}
+
+func absolutizePath(notebooksPath string) (string, error) {
+	absBookPath, err := filepath.Abs(notebooksPath)
+	if err != nil {
+		return "", errors.Wrapf(err, "Could not determine absolute path for \"%s\"", notebooksPath)
+	}
+
+	return absBookPath, nil
 }
